29699d3efc9d4d375e377358e41d45e93f591ce2
jtj <jtj1019@meatbox.co.kr>
2022-04-03 00:50:17 +0900

    #project init

---
+++ src/test/java/hello/core/empty/FirstTest.java
@@ -0,0 +1,16 @@
+package hello.core.empty;
+
+import org.junit.jupiter.api.Test;
+
+public class FirstTest {
+
+    @Test
+    public void test() {
+        System.out.println("hello world");
+
+
+    }
+
+
+
+}
