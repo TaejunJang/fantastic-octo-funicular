dcaa5fbce046c59bb928b5777e44d6f08c3e5506
jtj <jtj1019@meatbox.co.kr>
2022-04-03 01:39:03 +0900

    #멤버 서비스의 역할과 구현의 분리 작성

---
+++ src/main/java/hello/core/MemberApp.java
@@ -0,0 +1,20 @@
+package hello.core;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+
+public class MemberApp {
+    public static void main(String[] args) {
+        //메인으로 테스트하는건 이제 좋지 않은 방법이에여~~
+        //Junit을 통한 테스트 코드를 작성해요
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L,"memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("member = " + member.getName());
+        System.out.println("findMember = " + findMember.getName());
+    }
+}
